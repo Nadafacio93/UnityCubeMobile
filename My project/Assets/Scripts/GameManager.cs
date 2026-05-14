3d48b23c1ed5581de9e53a434c3c309eae01f449
Nadafacio93 <larthurnb@gmail.com>
2026-05-13 21:54:59 -0300

    UnityCubeMobile

--- My project/Assets/Scripts/GameManager.cs
+++ My project/Assets/Scripts/GameManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,27 +10,86 @@ public class GameManager : MonoBehaviour
     public float spawnY = 11f;
     public float spawnX = 7f;
 
+    [SerializeField] private InputActionReference cancelAction;
+
+    private void OnEnable()
+    {
+        cancelAction.action.Enable();
+        // Registrar evento
+        cancelAction.action.performed += OnCancel;
+    }
+
+    private void OnDisable()
+    {
+        // Remover evento
+        cancelAction.action.performed -= OnCancel;
+        cancelAction.action.Disable();
+    }
+
+    private void OnCancel(InputAction.CallbackContext context)
+    {
+        if (Time.timeScale == 0f)
+        {
+            StartCoroutine(ScaleTime(0f, 1f, 0.5f));
+        }
+        else if (Time.timeScale == 1f)
+        {
+            StartCoroutine(ScaleTime(1f, 0f, 0.5f));
+        }
+    }
+
     void Start()
     {
         StartCoroutine(SpawnObstacle());
     }
 
+
     private IEnumerator SpawnObstacle()
     {
         while (!isGameOver)
         {
+
             var obstacleSpawn = Random.Range(1, 4);
-            for(int i = 0; i < obstacleSpawn; i++)
+            for (int i = 0; i < obstacleSpawn; i++)//Cria laço para spawnar mais de um obstáculo por vez
             {
+
                 var xPosition = Random.Range(-spawnX, spawnX);
-                var damping = Random.Range(0f, 2f);
-                var objObstacle = Instantiate(obstaclePrefab, new Vector3(xPosition, spawnY, 0), Quaternion.identity);
+
+                var damping = Random.Range(0f, 2f);//Resitência do AR no objeto
+
+                var objObstacle =
+                Instantiate(obstaclePrefab,
+                new Vector3(xPosition, spawnY, 0), Quaternion.identity);
 
                 objObstacle.GetComponent<Rigidbody>().linearDamping = damping;
             }
-            
 
             yield return new WaitForSeconds(spawnInterval);
         }
     }
+
+    private IEnumerator ScaleTime(float start, float end, float duration)
+    {
+        float lastTime = Time.realtimeSinceStartup;
+        float timer = 0.0f;
+
+        while (timer < duration)
+        {
+            //Interpolação e suavisação do tempo
+            Time.timeScale = Mathf.Lerp(start, end, timer / duration);
+
+            //Ajustar e manter a concistência do tempo de física
+            Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+            timer += Time.realtimeSinceStartup - lastTime;
+            lastTime = Time.realtimeSinceStartup;
+
+            yield return null;
+        }
+
+        Time.timeScale = end;
+        Time.fixedDeltaTime = 0.02f * Time.timeScale;
+
+    }
+
 }
--- My project/Assets/Scripts/Obstacle.cs
+++ My project/Assets/Scripts/Obstacle.cs
@@ -1,23 +1,35 @@
+using Unity.Cinemachine;
 using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
     private Vector3 rotation;
     public ParticleSystem destructionParticle;
+    private CinemachineImpulseSource _impulseSource;
+    private PlayerController playerController;
     private void Start()
     {
-        var xRotation = Random.Range(0.5f, 1f);
+        var xRotation = Random.Range(90f, 180f);
         rotation = new Vector3(xRotation, 0);
+        _impulseSource = GetComponent<CinemachineImpulseSource>();
+        playerController = FindAnyObjectByType<PlayerController>();
     }
     private void Update()
     {
-        transform.Rotate(rotation);
+        transform.Rotate(rotation * Time.deltaTime);
     }
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag("Obstacle"))
         {
             Instantiate(destructionParticle, transform.position, Quaternion.identity);
+
+            if(playerController != null)
+            {
+                var distance = Vector3.Distance(transform.position, playerController.transform.position);
+                var force = 1 / distance;
+                _impulseSource.GenerateImpulse(force);
+            }
             Destroy(gameObject);
         }
     }
